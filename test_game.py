1de5b53093bf33f051532fe924f75d5540cbcf7a
geony_kkk <blueds05@gmail.com>
2024-07-10 14:07:29 +0900

    [feature] 30page 3자리 검사 로직 추가

--- test_game.py
+++ test_game.py
@@ -5,9 +5,13 @@ from game import Game
 
 class TestGame(TestCase):
     def setUp(self) -> None:
+        self.game = Game()
         super().setUp()
 
     def test_exception_when_input_is_none(self):
-        self.game = Game()
         with self.assertRaises(TypeError):
             self.game.guess(None)
+
+    def test_exception_when_input_length_is_unmatched(self):
+        with self.assertRaises(TypeError):
+            self.game.guess("12")
