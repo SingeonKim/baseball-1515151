bb7ecaf93485b15e142594c6c8b75af12749daf8
geony_kkk <blueds05@gmail.com>
2024-07-10 14:52:20 +0900

    [feature] 60page, Strike 세는 기능 구현

--- test_game.py
+++ test_game.py
@@ -29,13 +29,17 @@ class TestGame(TestCase):
         self.assertEqual(strikes, result.get_strikes())
         self.assertEqual(balls, result.get_balls())
 
-    def test_return_solve_result_if_matched_number(self):
-        self.generate_question("123")
-        self.assert_matched_number(self.game.guess("123"), True, 3, 0)
-
     def test_return_solve_result_if_unmatched_number(self):
         self.generate_question("123")
         self.assert_matched_number(self.game.guess("456"), False, 0, 0)
 
     def generate_question(self, question_number):
         self.game.question = question_number
+
+    def test_return_solve_result_if_matched_number(self):
+        self.generate_question("123")
+        self.assert_matched_number(self.game.guess("123"), True, 3, 0)
+
+    def test_return_solve_result_if_some_matched_number(self):
+        self.generate_question("123")
+        self.assert_matched_number(self.game.guess("120"), False, 2, 0)
