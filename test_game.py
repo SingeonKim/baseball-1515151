ca731a59ff04d1c2d350f2c30ca2ce9238107ab1
geony_kkk <blueds05@gmail.com>
2024-07-10 14:05:05 +0900

    [refact] 28page naming 변경 및 중복 코드 추출

--- test_game.py
+++ test_game.py
@@ -4,6 +4,9 @@ from game import Game
 
 
 class TestGame(TestCase):
+    def setUp(self) -> None:
+        super().setUp()
+
     def test_exception_when_input_is_none(self):
         self.game = Game()
         with self.assertRaises(TypeError):
